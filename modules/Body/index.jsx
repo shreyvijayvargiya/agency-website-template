1a6a16605d7f0242b269f302fd1a0cfa850a62d9
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2025-09-21 12:37:51 +0530

    buy template button added!

--- modules/Body/index.jsx
+++ modules/Body/index.jsx
@@ -5,6 +5,15 @@ const Body = ({ children }) => {
 	return (
 		<>
 			<Navbar />
+			<div className="fixed bottom-4 text-zinc-800 bg-zinc-50 text-sm hover:text-black hover:scale-105 transition-all duration-100 ease-in right-4 px-4 py-2">
+				<a
+					href="https://shreyvijayvargiya.gumroad.com/l/agency-client-website-template?layout=profile"
+					target="_blank"
+					className=""
+				>
+					Buy template
+				</a>
+			</div>
 			{children}
 		</>
 	);
